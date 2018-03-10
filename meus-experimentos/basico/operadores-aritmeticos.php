6f3fcd2c229958aec161f6c9795b89ce815ebb87
Adriano Maciel <adriano_mail@hotmail.com>
2018-03-09 22:13:03 -0300

    - adicionados experimentos de sequência aritmética

--- meus-experimentos/basico/operadores-aritmeticos.php
+++ meus-experimentos/basico/operadores-aritmeticos.php
@@ -18,3 +18,33 @@ Sequência, dois itens na mesma linha, é o que aparecer primeiro:
 **
 *, /
 +, -
+
+<?php
+    echo 5 / 3;
+    echo chr(13).chr(10);
+    echo 3 ** 8;
+    echo chr(13).chr(10);
+    echo 8 * 7;
+    echo chr(13).chr(10);
+    echo 7 - 1;
+    echo chr(13).chr(10);
+    echo 1 + 3;
+    echo chr(13).chr(10);
+    echo chr(13).chr(10);
+    echo 5 / 3 ** 8; // primeiro expoente
+    echo chr(13).chr(10);
+    echo 8 * 7 - 5 / 3; // primeiro a multiplicacao, divisao e subtração
+    echo chr(13).chr(10);
+    echo 5 / 3 / 8 * 7; // primeiro a divisão e depois a multiplicação
+    echo chr(13).chr(10);
+    echo 9 ** 5 / 6 ** 6; // primeiro a potência
+    echo chr(13).chr(10);
+    echo 15 % 7;
+    echo chr(13).chr(10);
+    echo 12 % 7;
+    echo chr(13).chr(10);
+    echo 15 % 7 / 12 % 7; // em sequencia, o módulo não define prioridade
+    echo chr(13).chr(10);
+    echo 0.083333333333333 % 7;
+    echo chr(13).chr(10);
+?>
